1b6f820287a062cfeef3ed0b4cb80aac31f40c5f
usent <usent@172.19.12.187>
2024-10-15 08:44:17 +0300

    first

---
+++ 1.ders/src/ders/ders1.java
@@ -0,0 +1,45 @@
+package ders;
+
+public class ders1 {
+	int h;
+	float y;
+	
+
+	ders1(int h, float y) {
+		this.h = h;
+		this.y = y;
+	}
+	
+
+
+	protected int getH() {
+		return h;
+	}
+
+
+
+	protected void setH(int h) {
+		this.h = h;
+	}
+
+
+
+	protected float getY() {
+		return y;
+	}
+
+
+
+	protected void setY(float y) {
+		this.y = y;
+	}
+
+
+
+	public static void main(String[] args) {
+		
+		System.out.println("Merhaba Dünya");
+
+	}
+
+}
---
+++ 1.ders/src/ders/ders1v1.java
@@ -0,0 +1,17 @@
+package ders;
+
+import java.util.Scanner;
+
+public class ders1v1 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=a+b;
+		System.out.println(c);
+		ders1 x=new ders1(3,325);
+	
+	}
+
+}
